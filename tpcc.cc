1c6d800371e7ae72362914446f8784c4ea35952c
yiqiao <yimingqiao3163@gmail.com>
2023-02-24 17:36:09 +0800

    bug fix, 1 millin txns.

--- tpcc.cc
+++ tpcc.cc
@@ -14,7 +14,7 @@
 #include "tpccgenerator.h"
 #include "tpcctables.h"
 
-static const int NUM_TRANSACTIONS = 100000;
+static const int NUM_TRANSACTIONS = 1000000;
 static const int kTxnsInterval = 5000;
 
 enum Mode {
